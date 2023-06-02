93162e01fa6c9936baa51192a225bcb8ccfe72ba
Noam Ackerman <developer.noam@gmail.com>
2023-06-02 18:55:21 +0300

    trying to fix scroll behaviour on url changes

--- src/components/scrollToTop.js
+++ src/components/scrollToTop.js
@@ -5,7 +5,9 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    setTimeout(function () {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }, 100);
   }, [pathname]);
 
   return null;
