05be6c5a8ca02e09554f289d27b812a484f57494
Noam Ackerman <developer.noam@gmail.com>
2023-07-31 12:26:14 +0300

    Improving filtering in users search page, using useMemo instead of unnecessary useState and useEffect

--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -1,64 +1,79 @@
 import React, { useState } from "react";
 import { useUsersCtx } from "../context/usersContext";
 import { Link } from "react-router-dom";
+import { Hearts } from "react-loader-spinner";
 import styles from "../style-modules/style.module.css";
 
 export default function Search() {
   const { usersData } = useUsersCtx();
   const [searchQuery, setsearchQuery] = useState("");
-  const [scoredUsers, setScoredUsers] = useState([]);
   const searchInput = React.useRef();
 
-  React.useEffect(() => {
-    if (searchQuery.trim() === "") {
-      setScoredUsers([]);
-    } else {
-      const usersArray = usersData ? Object.values(usersData) : [];
-      const filteredUsers = usersArray.filter(
-        (user) =>
-          user.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          user.displayName.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-      setScoredUsers(filteredUsers);
-    }
-  }, [searchQuery, usersData]);
+  const scoredUsers = React.useMemo(() => {
+    if (searchQuery.trim() === "") return [];
+    return Object.values(usersData).filter(
+      (user) =>
+        user.email.toLowerCase().includes(searchQuery.toLowerCase().trim()) ||
+        user.displayName
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase().trim())
+    );
+  }, [usersData, searchQuery]);
+
+  console.log(scoredUsers);
 
   function handleChange(e) {
     setsearchQuery(searchInput.current.value);
   }
 
   return (
-    <div className={styles.SearchPageContainter}>
-      <div className={styles.SearchTitle}>Search Users</div>
-      <input
-        className={styles.searchInput}
-        type="text"
-        ref={searchInput}
-        name="search"
-        placeholder="Search username/email"
-        onChange={handleChange}
-      />
-      <div className={styles.searchResultsWrapper}>
-        {scoredUsers.length > 0 &&
-          scoredUsers.map((user, index) => {
-            return (
-              <Link
-                to={`/${user.userId}`}
-                key={index}
-                className={styles.userCard}
-              >
-                <div className={styles.profileImgLargerThumbnailWrapper}>
-                  <img
-                    className={styles.profileImgThumbnail}
-                    src={user.photoURL}
-                    alt={user.email}
-                  />
-                </div>
-                <div className={styles.usernameResult}>{user.displayName}</div>
-              </Link>
-            );
-          })}
-      </div>
-    </div>
+    <>
+      {usersData ? (
+        <div className={styles.SearchPageContainter}>
+          <div className={styles.SearchTitle}>Search Users</div>
+          <input
+            className={styles.searchInput}
+            type="text"
+            ref={searchInput}
+            name="search"
+            placeholder="Search username/email"
+            onChange={handleChange}
+          />
+          <div className={styles.searchResultsWrapper}>
+            {scoredUsers.length > 0 &&
+              scoredUsers.map((user, index) => {
+                return (
+                  <Link
+                    to={`/${user.userId}`}
+                    key={index}
+                    className={styles.userCard}
+                  >
+                    <div className={styles.profileImgLargerThumbnailWrapper}>
+                      <img
+                        className={styles.profileImgThumbnail}
+                        src={user.photoURL}
+                        alt={user.email}
+                      />
+                    </div>
+                    <div className={styles.usernameResult}>
+                      {user.displayName}
+                    </div>
+                  </Link>
+                );
+              })}
+          </div>
+        </div>
+      ) : (
+        <Hearts
+          height="200"
+          width="200"
+          color="#B5A1FF"
+          ariaLabel="hearts-loading"
+          wrapperStyle={{}}
+          wrapperClass={styles.heartsPageLoader}
+          visible={true}
+        />
+      )}
+    </>
   );
 }
